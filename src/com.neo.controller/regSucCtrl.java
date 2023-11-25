f6e9143b4e2d78946081829ca7c2b27666bc80df
NEOivy11 <141548345+NEOivy11@users.noreply.github.com>
2023-11-25 21:03:54 +0800

    Add files via upload

---
+++ src/com.neo.controller/regSucCtrl.java
@@ -0,0 +1,41 @@
+package com.neo.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.neo.entity.User;
+import com.neo.service.userService;
+
+
+@WebServlet("/success")
+public class regSucCtrl extends HttpServlet {
+
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String userName = (String) request.getSession().getAttribute("UserName");
+		
+		userService userService = new userService();
+		String nickname = userService.findNickname(userName);
+		request.setAttribute("nickname", nickname);
+		User USER = userService.findByUserName(userName);
+		request.setAttribute("isAdmin", USER.getAdminUser());
+		request.setAttribute("UserName", userName);
+		request.getRequestDispatcher("/regSuccess.jsp").forward(request, response);
+		
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
