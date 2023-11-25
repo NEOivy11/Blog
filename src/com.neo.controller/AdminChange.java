d7249ccf82fb8e447eabdb397d0b1fcdb51628d9
NEOivy11 <141548345+NEOivy11@users.noreply.github.com>
2023-11-25 20:33:00 +0800

    Create AdminChange.java

---
+++ src/com.neo.controller/AdminChange.java
@@ -0,0 +1,41 @@
+package com.neo.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.neo.entity.User;
+import com.neo.service.userService;
+
+@WebServlet("/ChangeAdmin")
+public class AdminChange extends HttpServlet {
+	
+		public void doGet(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			
+			String userName = (String)request.getSession().getAttribute("UserName");
+			
+			if (userName == null || userName.equals("")){
+				response.sendRedirect("Login");
+				return;
+			}
+				
+			String UserName = request.getParameter("UserName");
+			userService userService = new userService();
+			User user = userService.findByUserName(UserName);
+			user = userService.AlterAdmin(user,userName);		
+			
+			response.sendRedirect("UserManage");
+			
+		}
+
+		
+		public void doPost(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			doGet(request, response);
+		}
+	}
